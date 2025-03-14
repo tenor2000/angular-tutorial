d3e0d4e438d7bd80e25111c59d8864dc6c1746f5
tenor2000 <tenor2000@gmail.com>
2025-03-14 10:23:55 -0400

    setup example pages

---
+++ src/app/contact-home/contact-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-home.component.html',
+  styleUrl: './contact-home.component.css'
+})
+export class ContactHomeComponent {
+
+}
---
+++ src/app/home-home/home-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-home.component.html',
+  styleUrl: './home-home.component.css'
+})
+export class HomeHomeComponent {
+
+}
