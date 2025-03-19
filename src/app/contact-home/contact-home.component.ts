9cf2050c897cdbb54c5d70d6acaa10730bf6a1e6
tenor2000 <tenor2000@gmail.com>
2025-03-19 15:52:12 -0400

    made the example forms

--- src/app/contact-home/contact-home.component.ts
+++ src/app/contact-home/contact-home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-home',
   standalone: true,
   imports: [],
   templateUrl: './contact-home.component.html',
-  styleUrl: './contact-home.component.css'
+  styleUrl: './contact-home.component.css',
 })
-export class ContactHomeComponent {
+export class ContactHomeComponent implements OnInit {
+  isUnchanged = true;
 
+  onCancel(event?: KeyboardEvent) {
+    const evtMsg = event
+      ? ' Event target is ' + (event.target as HTMLElement).textContent
+      : '';
+    alert('Cancelled.' + evtMsg);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
