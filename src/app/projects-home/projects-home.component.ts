8211681a6e0ec4bc0a375bc568b29f41f069a675
tenor2000 <tenor2000@gmail.com>
2025-03-20 10:51:18 -0400

    completed 5.9

--- src/app/projects-home/projects-home.component.ts
+++ src/app/projects-home/projects-home.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects-home',
   standalone: true,
   imports: [],
   templateUrl: './projects-home.component.html',
-  styleUrl: './projects-home.component.css'
+  styleUrl: './projects-home.component.css',
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  firstName: string = 'William';
+  lastName: string = 'Wallace';
+  phoneNumber: string = '(201) 748-6000';
 
+  setProperty(event: Event) {
+    let target = event.target as HTMLInputElement;
+    let property: string = target.id;
+    switch (property) {
+      case 'firstNameInput':
+        this.firstName = target.value;
+        break;
+      case 'lastNameInput':
+        this.lastName = target.value;
+        break;
+      case 'phoneNumberInput':
+        this.phoneNumber = target.value;
+        break;
+      default:
+        console.warn('No Matching ID Found!');
+        break;
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
